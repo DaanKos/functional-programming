86da8339e541d9f6de50b9566515914a1561aa49
Daan <43338443+DaanKos@users.noreply.github.com>
2019-11-13 09:24:08 +0100

    Cleaned up code, removed ugly consts

--- src/app.js
+++ src/app.js
@@ -1,25 +1,6 @@
 import formatData from "./utils/formatData";
 import queryCollection from "./utils/queryCollection";
-
-const popularcultureTermMasterId = 10045782;
-const jachtvisserijvoedselgaringTermMasterId = 2803;
-const wapenTermMasterId = 2091;
-const landtuinenbosbouwTermMasterId = 2819;
-const veeteeltenproductenTermMasterId = 1843;
-const voedingdrankgenotmiddelenTermMasterId = 2839;
-const kledingenpersoonlijkeversieringTermMasterId = 2704;
-const lichaamsverzorginggeneeskundepersoonlijkcomfortTermMasterId = 2718;
-const vestigingTermMasterId = 2726;
-const nijverheidhandelendienstverleningTermMasterId = 2754;
-const vervoerTermMasterId = 2624;
-const communicatieTermMasterId = 2634;
-const sociaalpolitiekjuridischTermMasterId = 2642;
-const levenscyclusTermMasterId = 2649;
-const religieenritueelTermMasterId = 2652;
-const kunstTermMasterId = 2657;
-const ontspanningsportenspelTermMasterId = 2676;
-const onbepaaldTermMasterId = 1834;
-const strijdenoorlogTermMasterId = 16239;
+import categoryArray from "./utils/categoryArray";
 
 function runQuery(mainCategory, termMasterId) {
     // The following piece of code was written by user Razpudding (Laurens), from https://codepen.io/Razpudding/pen/LKMbwZ
@@ -39,26 +20,9 @@ function runQuery(mainCategory, termMasterId) {
       let results = json.results.bindings;
       console.log("Following array is based on: ", mainCategory);
       console.log(formatData(mainCategory, results))
+      console.log("The original array of ", mainCategory, " was ", results);
       })
   
 }
 
-runQuery("mainPopularCulture", popularcultureTermMasterId);
-runQuery("mainJachtVisserijVoedselgaring", jachtvisserijvoedselgaringTermMasterId);
-runQuery("mainWapens", wapenTermMasterId);
-runQuery("mainLandTuinEnBosbouw", landtuinenbosbouwTermMasterId);
-runQuery("mainVeeteeltEnProducten", veeteeltenproductenTermMasterId);
-runQuery("mainVoedingDrankGenotmiddelen", voedingdrankgenotmiddelenTermMasterId);
-runQuery("mainKledingEnPersoonlijkeversiering", kledingenpersoonlijkeversieringTermMasterId);
-runQuery("mainLichaamsVerzorgingGeneeskundePersoonlijkcomfort", lichaamsverzorginggeneeskundepersoonlijkcomfortTermMasterId);
-runQuery("mainVestiging", vestigingTermMasterId);
-runQuery("mainNijverheidHandelEnDienstverlening", nijverheidhandelendienstverleningTermMasterId);
-runQuery("mainVervoer", vervoerTermMasterId);
-runQuery("mainCommunicatie", communicatieTermMasterId);
-runQuery("mainSociaalPolitiekJuridisch", sociaalpolitiekjuridischTermMasterId);
-runQuery("mainLevenscyclus", levenscyclusTermMasterId);
-runQuery("mainReligieEnRitueel", religieenritueelTermMasterId);
-runQuery("mainKunst", kunstTermMasterId);
-runQuery("mainOntspanningSportEnSpel", ontspanningsportenspelTermMasterId);
-runQuery("mainOnbepaald", onbepaaldTermMasterId);
-runQuery("mainStrijdEnOorlog", strijdenoorlogTermMasterId);
+categoryArray.map(categoryItem => runQuery(categoryItem.categoryName, categoryItem.termMasterId));
