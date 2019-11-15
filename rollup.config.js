9fec43a814377032bc060ad61f6eaba7a20d5160
Daan <43338443+DaanKos@users.noreply.github.com>
2019-11-15 09:19:45 +0100

    Removed unnecessary files

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,8 @@
-// import babel from 'rollup-plugin-babel'
-// import commonjs from 'rollup-plugin-commonjs'
-// import resolve from 'rollup-plugin-node-resolve'
-// import { terser } from 'rollup-plugin-terser'
-
 export default {
     input: 'src/app.js',
     output: {
         file: 'public/scripts/bundle.js',
         format: 'iife',
         name: 'bundle',
-    },
-    plugins: [
-        // babel({
-        //     exclude: 'node_modules/**'
-        // }),
-        // resolve({
-        //     main: true,
-        //     browser: true
-        // }),
-        // commonjs(),
-        // terser()
-    ]
+    }
 }
